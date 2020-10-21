00c528c0f8359df0bc82bd96cb6cfc949c2e1642
lefmejia <lefmejia@unitec.edu>
2020-10-20 20:06:45 -0600

    commit 2

---
+++ arreglo/array.h
@@ -0,0 +1,45 @@
+#ifndef _ARRAY_H_
+#define _ARRAY_H_
+
+#include <initializer_list>
+
+template <typename TElem>
+class Array
+{
+public:
+    Array(const std::initializer_list<TElem>& init);
+    Array(int size);
+    Array(const Array<TElem> &arr2);
+
+    ~Array()
+    { delete [] arr; }
+
+    TElem& operator[](unsigned idx)
+    { return arr[idx]; }
+
+    Array& operator=(const Array& rhs);
+
+    void print();
+    int find(TElem element);
+    int findMinIndex(int pos);
+    int findMaxIndex(int pos);
+
+    TElem findMinElement()
+    { return arr[findMinIndex(0)]; }
+    
+    TElem findMaxElement()
+    { return arr[findMaxIndex(0)]; }
+
+    void sort();
+    void insertionSort();
+    int size()
+    { return size_; }
+
+private:
+    int size_;
+    TElem *arr;
+};
+
+#include "array.cpp"
+
+#endif
